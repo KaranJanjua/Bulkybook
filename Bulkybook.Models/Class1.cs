62b035e4b8495065c8a7e1b498dc9c0528ea4fc2
Karanbir Singh Janjua <karanbirjanjua@gmail.com>
2022-04-27 10:27:12 +0930

    Added class library's to the project - DataAccess - Models - Utility

---
+++ Bulkybook.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Bulkybook.Models
+{
+    public class Class1
+    {
+    }
+}
---
+++ Bulkybook.Utility/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Bulkybook.Utility
+{
+    public class Class1
+    {
+    }
+}
